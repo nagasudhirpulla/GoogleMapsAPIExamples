83b25a3dae96ebfb167b65859c4f23678569d19a
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-05-06 22:32:09 +0530

    minor mistake

--- pmucontour.js
+++ pmucontour.js
@@ -21,8 +21,8 @@ document.onreadystatechange = function () {
         pmuVisualizer = new AwesomePMUCanvasController(
             {
                 'consoleWriteFunction': WriteLineConsole,
-                'computingExpressFunction' : function(){ document.getElementById("wrapper").style.border = "2px solid rgb(255,0,0)"; };
-                'computingStopExpressFunction' : function(){ document.getElementById("wrapper").style.border = "2px solid rgb(0,255,0)"; };
+                'computingExpressFunction' : function(){ document.getElementById("wrapper").style.border = "2px solid rgb(255,0,0)"; },
+                'computingStopExpressFunction' : function(){ document.getElementById("wrapper").style.border = "2px solid rgb(0,255,0)" };
             }
         );
 
