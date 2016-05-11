ec8870e2e3b0f498689fc08c6168899e6b6093f0
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-05-11 17:13:54 +0530

    adding only status ok voltage points to table

--- pmucontour.js
+++ pmucontour.js
@@ -73,7 +73,7 @@ function addVoltPointsToTable() {
     clearTable();
     var sources = pmuVisualizer.getVoltagePoints();
     for (var i = 0; i < sources.length; i++) {
-        if(sources[i].status = "OK"){
+        if(sources[i][6] == "OK"){
             addRow(i + 1, sources[i][5], sources[i][0], sources[i][1], (sources[i][2]).toFixed(4));
         }
     }
